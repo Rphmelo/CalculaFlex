601c3edfe44d3582f30b4259f3391910d5093ee3
Rphmelo <rphmelo55@gmail.com>
2018-12-03 21:23:58 -0200

    add calculate functionality

--- app/src/main/java/br/com/rphmelo/calculaflex/FormActivity.kt
+++ app/src/main/java/br/com/rphmelo/calculaflex/FormActivity.kt
@@ -3,6 +3,8 @@ package br.com.rphmelo.calculaflex
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.rphmelo.calculaflex.extensions.getDouble
+import br.com.rphmelo.calculaflex.watchers.DecimalTextWatcher
 import kotlinx.android.synthetic.main.activity_form.*
 
 class FormActivity : AppCompatActivity() {
@@ -11,13 +13,18 @@ class FormActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
 
+        etGasPrice.addTextChangedListener(DecimalTextWatcher(etGasPrice))
+        etEthanolPrice.addTextChangedListener(DecimalTextWatcher(etEthanolPrice))
+        etGasAverage.addTextChangedListener(DecimalTextWatcher(etGasAverage))
+        etEthanolAverage.addTextChangedListener(DecimalTextWatcher(etEthanolAverage))
+
         btCalculate.setOnClickListener {
-            val proximaTela = Intent(this, ResultActivity::class.java)
-            proximaTela.putExtra("GAS_PRICE", etGasPrice.text.toString())
-            proximaTela.putExtra("ETHANOL_PRICE", etEthanolPrice.text.toString())
-            proximaTela.putExtra("GAS_AVERAGE", etGasAverage.text.toString())
-            proximaTela.putExtra("ETHANOL_AVERAGE", etEthanolAverage.text.toString())
-            startActivity(proximaTela)
+            val nextScreen = Intent(this, ResultActivity::class.java)
+            nextScreen.putExtra("GAS_PRICE", etGasPrice.getDouble())
+            nextScreen.putExtra("ETHANOL_PRICE", etEthanolPrice.getDouble())
+            nextScreen.putExtra("GAS_AVERAGE", etGasAverage.getDouble())
+            nextScreen.putExtra("ETHANOL_AVERAGE", etEthanolAverage.getDouble())
+            startActivity(nextScreen)
         }
     }
 }
--- app/src/main/java/br/com/rphmelo/calculaflex/ResultActivity.kt
+++ app/src/main/java/br/com/rphmelo/calculaflex/ResultActivity.kt
@@ -2,11 +2,44 @@ package br.com.rphmelo.calculaflex
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.rphmelo.calculaflex.extensions.format
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(false)
+        calculate()
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+    private fun calculate() {
+        val gasPrice = intent.extras.getDouble("GAS_PRICE", 0.0)
+        val ethanolPrice = intent.extras.getDouble("ETHANOL_PRICE", 0.0)
+        val gasAverage = intent.extras.getDouble("GAS_AVERAGE", 0.0)
+        val ethanolAverage = intent.extras.getDouble("ETHANOL_AVERAGE", 0.0)
+
+        val carPerformance = ethanolAverage / gasAverage
+        val priceIndice = ethanolPrice / gasPrice
+
+        if(priceIndice <= carPerformance){
+            tvSuggestion.text = getString(R.string.label_ethanol)
+        } else {
+            tvSuggestion.text = getString(R.string.label_gasoline)
+        }
+
+        tvGasAverageResult.text = (gasPrice / gasAverage).format(2)
+        tvEthanolAverageResult.text = (ethanolPrice / ethanolAverage).format(2)
+
+        tvFuelRatio.text = getString(R.string.label_ratio_fuel, carPerformance.format(2))
+    }
+
+
 }
